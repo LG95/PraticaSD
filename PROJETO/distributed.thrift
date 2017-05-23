177a34f2da44757ef4ec15e30cfd0638b7f9c7fb
Luiz Gustavo Gomes Borges <luiz.g.borges95@gmail.com>
2017-05-23 00:01:03 -0300

    Modified create, update and delete methods to be one way.

--- PROJETO/distributed.thrift
+++ PROJETO/distributed.thrift
@@ -16,16 +16,16 @@ struct Edge {
 }
 
 service Graph {
-	bool createNode(1: Node node),
+	oneway void createNode(1: Node node),
 	Node readNode(1: i32 id),
-	bool updateNode(1: i32 id, 2: i32 color, 3: string description,
+	oneway void updateNode(1: i32 id, 2: i32 color, 3: string description,
 					4: double weight),
-	bool deleteNode(1: i32 id),
-	bool createEdge(1: Edge edge),
+	oneway void deleteNode(1: i32 id),
+	oneway void createEdge(1: Edge edge),
 	Edge readEdge(1: i32 node1, 2: i32 node2),
-	bool updateEdge(1: i32 node1, 2: i32 node2, 3: double weight,
+	oneway void updateEdge(1: i32 node1, 2: i32 node2, 3: double weight,
 					4: bool direction, 5: string description),
-	bool deleteEdge(1: i32 node1, 2: i32 node2),
+	oneway void deleteEdge(1: i32 node1, 2: i32 node2),
 	list<Node> listNodesEdge(1: i32 node1, 2: i32 node2),
 	list<Edge> listEdgesNode(1: i32 id),
 	list<Node> listNeighborNodes(1: i32 id)

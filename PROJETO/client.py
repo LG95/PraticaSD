22c6c3d5d9606c02ad4cc0fe5f074ec9a060c5de
Luiz Gustavo Gomes Borges <luiz.g.borges95@gmail.com>
2017-05-23 22:28:50 -0300

    Added user input before each set of operations.

--- PROJETO/client.py
+++ PROJETO/client.py
@@ -7,11 +7,15 @@ def main(graphs):
 
 	ids = sample(xrange(1, 26), 10)
 
+	raw_input("Create?")
+
 	for node_id in ids:
 		graphs.createNode( Node(node_id, 0, "", 0.0) )
 
 	for _ in xrange(50):
 		graphs.createEdge( Edge(choice(ids), choice(ids), 0.0, False, "") )
+
+	raw_input("Update?")
 
 	for node_id in sample(ids, 5):
 		graphs.updateNode(node_id, 1, "*", 1.0)
@@ -19,11 +23,15 @@ def main(graphs):
 	for _ in xrange(25):
 		graphs.updateEdge(choice(ids), choice(ids), 1.0, True, "*")
 
+	raw_input("Delete?")
+
 	for node_id in sample(ids, 5):
 		graphs.deleteNode(node_id)
 
 	for _ in xrange(10):
 		graphs.deleteEdge(choice(ids), choice(ids))
+
+	raw_input("Read?")
 
 	for node_id in sample(ids, 5):
 		node = graphs.readNode(node_id)
@@ -32,6 +40,8 @@ def main(graphs):
 	for _ in xrange(25):
 		edge = graphs.readEdge(choice(ids), choice(ids))
 		if edge.node1 is not None: print(edge)
+
+	raw_input("List?")
 
 	for _ in xrange(50):
 		nodes = graphs.listNodesEdge(choice(ids), choice(ids))
@@ -58,10 +68,6 @@ if __name__ == '__main__':
 	client		= Client(protocol)
 
 	transport.open()
-	main(client)
-	transport.close()
-
-	# from server import GraphDatabaseHandler
-
-	# with GraphDatabaseHandler() as handler:
-	# 	main(handler)
+	try: main(client)
+	except (EOFError, KeyboardInterrupt): pass
+	finally: transport.close()

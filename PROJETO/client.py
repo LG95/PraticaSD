9310bc735232108f9f6e4e0f4fda8e06fa15eb6c
Luiz Gustavo Gomes Borges <luiz.g.borges95@gmail.com>
2017-05-24 22:50:53 -0300

    Added user input between operations. Created example function for a specific graph.

--- PROJETO/client.py
+++ PROJETO/client.py
@@ -5,6 +5,8 @@ def main(graphs):
 
 	from distributed.ttypes import Edge, Node
 
+	raw_input("Create?")
+
 	ids = sample(xrange(1, 26), 10)
 
 	for node_id in ids:
@@ -13,17 +15,23 @@ def main(graphs):
 	for _ in xrange(50):
 		graphs.createEdge( Edge(choice(ids), choice(ids), 0.0, False, "") )
 
+	raw_input("Update?")
+
 	for node_id in sample(ids, 5):
 		graphs.updateNode(node_id, 1, "*", 1.0)
 
 	for _ in xrange(25):
 		graphs.updateEdge(choice(ids), choice(ids), 1.0, True, "*")
 
+	raw_input("Delete?")
+
 	for node_id in sample(ids, 5):
 		graphs.deleteNode(node_id)
 
 	for _ in xrange(10):
 		graphs.deleteEdge(choice(ids), choice(ids))
+
+	raw_input("Read?")
 
 	for node_id in sample(ids, 5):
 		node = graphs.readNode(node_id)
@@ -32,6 +40,8 @@ def main(graphs):
 	for _ in xrange(25):
 		edge = graphs.readEdge(choice(ids), choice(ids))
 		if edge.node1 is not None: print(edge)
+
+	raw_input("List?")
 
 	for _ in xrange(50):
 		nodes = graphs.listNodesEdge(choice(ids), choice(ids))
@@ -44,6 +54,36 @@ def main(graphs):
 	for node_id in sample(ids, 5):
 		neighbors = graphs.listNeighborNodes(node_id)
 		if neighbors != []: print(neighbors)
+
+def example(client):
+	from distributed.ttypes import Edge, Node
+
+	raw_input("Create?")
+
+	client.createNode( Node(0, 0, "A", 0.0) )
+	client.createNode( Node(1, 0, "B", 0.0) )
+	client.createNode( Node(2, 0, "C", 0.0) )
+	client.createNode( Node(3, 0, "D", 0.0) )
+	client.createNode( Node(4, 0, "E", 0.0) )
+	client.createNode( Node(5, 0, "F", 0.0) )
+
+	client.createEdge( Edge(0, 1, 0.0, False, "A-B") )
+	client.createEdge( Edge(0, 2, 0.0, False, "A-C") )
+	client.createEdge( Edge(1, 2, 0.0, False, "B-C") )
+	client.createEdge( Edge(2, 3, 0.0, False, "C-D") )
+	client.createEdge( Edge(3, 4, 0.0, False, "D-E") )
+	client.createEdge( Edge(3, 5, 0.0, False, "D-F") )
+	client.createEdge( Edge(4, 5, 0.0, False, "E-F") )
+
+	raw_input("Read?")
+
+	for i in range(6):
+		print( client.readNode(i) )
+
+	raw_input("List?")
+
+	for i in range(6):
+		print( client.listNeighborNodes(i) )
 
 if __name__ == '__main__':
 	from thrift.transport.TSocket		 import TSocket
@@ -58,10 +98,5 @@ if __name__ == '__main__':
 	client		= Client(protocol)
 
 	transport.open()
-	main(client)
+	example(client)
 	transport.close()
-
-	# from server import GraphDatabaseHandler
-
-	# with GraphDatabaseHandler() as handler:
-	# 	main(handler)

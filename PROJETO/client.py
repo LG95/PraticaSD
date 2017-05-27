214d99318db3ea0fcdb9ce02a4d1d7e64e65aa5c
Luiz Gustavo Gomes Borges <luiz.g.borges95@gmail.com>
2017-05-27 14:32:59 -0300

    Minor improvements.

--- PROJETO/client.py
+++ PROJETO/client.py
@@ -1,18 +1,18 @@
-from __future__ import print_function, unicode_literals
+from __future__ import absolute_import, print_function, unicode_literals
 
-def main(graphs):
+from distributed.ttypes import Edge, Node
+
+def test(graphs):
 	from random import choice, sample
-
-	from distributed.ttypes import Edge, Node
 
 	raw_input("Create?")
 
-	ids = sample(xrange(1, 26), 10)
+	ids = sample(range(1, 26), 10)
 
 	for node_id in ids:
 		graphs.createNode( Node(node_id, 0, "", 0.0) )
 
-	for _ in xrange(50):
+	for _ in range(50):
 		graphs.createEdge( Edge(choice(ids), choice(ids), 0.0, False, "") )
 
 	raw_input("Update?")
@@ -20,7 +20,7 @@ def main(graphs):
 	for node_id in sample(ids, 5):
 		graphs.updateNode(node_id, 1, "*", 1.0)
 
-	for _ in xrange(25):
+	for _ in range(25):
 		graphs.updateEdge(choice(ids), choice(ids), 1.0, True, "*")
 
 	raw_input("Delete?")
@@ -28,7 +28,7 @@ def main(graphs):
 	for node_id in sample(ids, 5):
 		graphs.deleteNode(node_id)
 
-	for _ in xrange(10):
+	for _ in range(10):
 		graphs.deleteEdge(choice(ids), choice(ids))
 
 	raw_input("Read?")
@@ -37,13 +37,13 @@ def main(graphs):
 		node = graphs.readNode(node_id)
 		if node.id is not None: print(node)
 
-	for _ in xrange(25):
+	for _ in range(25):
 		edge = graphs.readEdge(choice(ids), choice(ids))
 		if edge.node1 is not None: print(edge)
 
 	raw_input("List?")
 
-	for _ in xrange(50):
+	for _ in range(50):
 		nodes = graphs.listNodesEdge(choice(ids), choice(ids))
 		if nodes != []: print(nodes)
 
@@ -55,48 +55,46 @@ def main(graphs):
 		neighbors = graphs.listNeighborNodes(node_id)
 		if neighbors != []: print(neighbors)
 
-def example(client):
-	from distributed.ttypes import Edge, Node
-
+def example(graphs):
 	raw_input("Create?")
 
-	client.createNode( Node(0, 0, "A", 0.0) )
-	client.createNode( Node(1, 0, "B", 0.0) )
-	client.createNode( Node(2, 0, "C", 0.0) )
-	client.createNode( Node(3, 0, "D", 0.0) )
-	client.createNode( Node(4, 0, "E", 0.0) )
-	client.createNode( Node(5, 0, "F", 0.0) )
+	graphs.createNode( Node(0, 0, "A", 0.0) )
+	graphs.createNode( Node(1, 0, "B", 0.0) )
+	graphs.createNode( Node(2, 0, "C", 0.0) )
+	graphs.createNode( Node(3, 0, "D", 0.0) )
+	graphs.createNode( Node(4, 0, "E", 0.0) )
+	graphs.createNode( Node(5, 0, "F", 0.0) )
 
-	client.createEdge( Edge(0, 1, 0.0, False, "A-B") )
-	client.createEdge( Edge(0, 2, 0.0, False, "A-C") )
-	client.createEdge( Edge(1, 2, 0.0, False, "B-C") )
-	client.createEdge( Edge(2, 3, 0.0, False, "C-D") )
-	client.createEdge( Edge(3, 4, 0.0, False, "D-E") )
-	client.createEdge( Edge(3, 5, 0.0, False, "D-F") )
-	client.createEdge( Edge(4, 5, 0.0, False, "E-F") )
+	graphs.createEdge( Edge(0, 1, 0.0, False, "A-B") )
+	graphs.createEdge( Edge(0, 2, 0.0, False, "A-C") )
+	graphs.createEdge( Edge(1, 2, 0.0, False, "B-C") )
+	graphs.createEdge( Edge(2, 3, 0.0, False, "C-D") )
+	graphs.createEdge( Edge(3, 4, 0.0, False, "D-E") )
+	graphs.createEdge( Edge(3, 5, 0.0, False, "D-F") )
+	graphs.createEdge( Edge(4, 5, 0.0, False, "E-F") )
 
 	raw_input("Read?")
 
 	for i in range(6):
-		print( client.readNode(i) )
+		print( graphs.readNode(i) )
 
 	raw_input("List?")
 
 	for i in range(6):
-		print( client.listNeighborNodes(i) )
+		print( graphs.listNeighborNodes(i) )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	from thrift.transport.TSocket		 import TSocket
 	from thrift.transport.TTransport	 import TBufferedTransport
 	from thrift.protocol.TBinaryProtocol import TBinaryProtocol
 
 	from distributed.Graph import Client
 
-	transport	= TSocket('localhost', 13579)
+	transport	= TSocket("localhost", 13579)
 	transport	= TBufferedTransport(transport)
 	protocol	= TBinaryProtocol(transport)
 	client		= Client(protocol)
 
 	transport.open()
-	example(client)
-	transport.close()
+	try: example(client)
+	finally: transport.close()
